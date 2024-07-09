871ecdc15bc56ac71125422a94eab25f86d3caa2
jeynihgenk25 <jeynihgenk25@gmail.com>
2024-07-09 15:01:37 +0000

    🔹 feat: adjust icon size based on breakpoint using useBreakpointValue

--- src/Pages/projects.tsx
+++ src/Pages/projects.tsx
@@ -1,12 +1,14 @@
-import { Box, Flex, Img, Link, Text } from "@chakra-ui/react";
+import { Box, Flex, Img, Link, Text, useBreakpointValue } from "@chakra-ui/react";
 import { Icons, Section, TittleEl, projectsInfo } from "../Components";
 
 export default function Projects() {
+    const isMobile = useBreakpointValue({ base: true, md: false })
+
     return (
         <Section>
             <TittleEl.tittleShape tittle="My Projects" subTittle="Here are the projects that I have worked on" />
 
-            <Flex w={'full'} mt={12}>
+            <Flex w={'full'} mt={12} px={{base: 3, md: 6}}>
                 {projectsInfo.map(project => (
                     <Box key={project.id}>
                         <Img src={project.img} alt={project.alt} border={'2px'} borderRadius={'12px'} />
@@ -14,10 +16,10 @@ export default function Projects() {
                             <Text as={'h2'}>{project.name}</Text>
                             <Flex gap={3}>
                                 <Link href={project.link.github} target={project.link.target} rel={project.link.rel}>
-                                    <Icons.github size={28} />
+                                    <Icons.github size={isMobile === true ? 24 : 28} />
                                 </Link>
                                 <Link href={project.link.live} target={project.link.target} rel={project.link.rel}>
-                                    <Icons.externalLink size={26} />
+                                    <Icons.externalLink size={isMobile === true ? 22 : 26} />
                                 </Link>
                             </Flex>
                         </Flex>
